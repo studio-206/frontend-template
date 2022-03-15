7804ddafded22293f9f5012a865100f5bb14670a
Karl Taylor <karl@karltaylor.dev>
2022-03-15 11:38:10 +0000

    fix util types

--- src/styles/stitches.config.ts
+++ src/styles/stitches.config.ts
@@ -1,5 +1,9 @@
 import { createStitches } from "@stitches/react";
 
+import type * as Stitches from "@stitches/react";
+
+type UtilValue = Stitches.ScaleValue<"space"> | number | string;
+
 export const {
   styled,
   css,
@@ -12,15 +16,22 @@ export const {
 } = createStitches({
   theme: {
     colors: {},
+    space: {},
   },
   media: {
     tablet: "(min-width: 768px)",
     desktop: "(min-width: 1024px)",
   },
   utils: {
-    mx: (value: number | string) => ({ marginLeft: value, marginRight: value }),
-    my: (value: number | string) => ({ marginTop: value, marginBottom: value }),
-    px: (value: number | string) => ({ paddingLeft: value, paddingRight: value }),
-    py: (value: number | string) => ({ paddingTop: value, paddingBottom: value }),
+    mx: (value: UtilValue) => ({ marginLeft: value, marginRight: value }),
+    my: (value: UtilValue) => ({ marginTop: value, marginBottom: value }),
+    px: (value: UtilValue) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: UtilValue) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
   },
 });
