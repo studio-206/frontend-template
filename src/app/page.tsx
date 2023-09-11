33a70520885a38bc2ce926bd1a45b419d956f87b
karl <karl@studio206.dev>
2023-09-11 14:46:36 +0100

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Studio 206 - Frontend Template",
+  description: "Studio 206 Frontend Template",
+};
+
 export default function Home() {
   return <main>Studio 206</main>;
 }
